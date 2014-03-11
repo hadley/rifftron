9b750586bdda91d5559e807e78e2f309eaf6ec64
hadley <h.wickham@gmail.com>
2014-03-11 16:49:22 -0500

    Try to run automatically

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,12 @@
 project_name <- function() {
   basename(normalizePath(getwd()))
 }
+
+on_cran <- function() {
+  !identical(Sys.getenv("NOT_CRAN"), "true")
+}
+
+on_travis <- function() {
+  identical(Sys.getenv("TRAVIS"), "true")
+}
+
---
+++ tests/rifftron.R
@@ -0,0 +1,5 @@
+library(rifftron)
+
+if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+  riff_package("rifftron")
+}
